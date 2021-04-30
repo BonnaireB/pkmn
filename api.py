aea9589e6cc5f6a9079b22cfd1bffddce544aaa2
Bonnaire <bbonnaire@deloitte.ca>
2021-04-30 12:40:48 -0400

    A simple api with basic functions

---
+++ api.py
@@ -0,0 +1,98 @@
+import flask,json
+from flask import jsonify,request, abort
+import parse_data as p
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+#Retrieves the list of pokemons and the research parameters which are the different caracteristics of a pokemon
+pokemons,parameters = p.parse_data() 
+
+with open("ressources\\pokemon.json", "r", encoding="utf-8-sig")as j:
+    pokemons = json.load(j)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>POKEMON LIBRARY</h1><p>This site is a prototype API for accessing data about Pokemons.</p>"
+
+@app.route('/api/v1/resources/pokemon/all', methods=['GET'])
+def api_all_pokemon():
+    return jsonify(pokemons)
+
+@app.route('/api/v1/resources/pokemon/<int:pokemon_id>', methods=['GET'])    
+def api_id(pokemon_id):
+    pokes = [poke for poke in pokemons if str(poke['#']) == str(pokemon_id)]
+    if len(pokes) == 0:
+        abort(404)
+    return jsonify(pokes[0])
+
+@app.route('/api/v1/resources/pokemon', methods=['GET'])
+def api_pokemon():
+    results=[]
+    error = False
+    error_msg = "Impossible top find resource: \n"
+    print(request.args)
+    if not request.args:
+        return "Invalid research arguments"
+    for a in request.args:
+    #Look for each request parameters to see if they match available query parameters
+        if a in parameters.keys():
+    #If parameter is available, returns all the pokemon that matches the values
+            z=[poke for poke in pokemons if str(poke[parameters[a]]) == str(request.args[a])]
+            results += z
+
+        else :
+            # If the argument is not available, error message is also returned
+            error = True 
+            abort(404)
+            error_msg += "Invalid research argument "+ a+", available arguments are "+ str(list(parameters.keys()))+"\n"
+    if len(results) == 0:
+        abort(404)
+    r = list({v['#']:v for v in results}.values())
+    return error_msg if error else jsonify(r)
+
+@app.route('/api/v1/resources/pokemon', methods=['POST'])
+def add_pokemon():
+    poke = {
+        '#': str(int(pokemons[-1]['#']) + 1),
+        'id': str(int(pokemons[-1]['id']) + 1)
+    }
+    for a in parameters.keys():
+        b = parameters[a]
+        # If a parameter is missing in the list of parameters, then an error is raised
+        if b in request.json and type(pokemons[-1][b]) == type(request.json[b]):
+            poke[b] = request.json[b]
+        else : 
+            print(b)
+            abort(400)
+    check_duplicates(poke)
+    pokemons.append(poke)
+    return jsonify({'Pokemon': poke}), 201
+
+@app.route('/api/v1/resources/pokemon/<int:pokemon_id>', methods=['PUT'])
+def update_task(pokemon_id):
+    pokes = [poke for poke in pokemons if str(poke['#']) == str(pokemon_id)]
+    if len(pokes) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    for a in request.json:
+        if a in pokemons[0].keys() and type(request.json[a]) == type(pokemons[0][a]):
+            pokes[0][a] = request.json[a]
+        else : abort(400)
+    return jsonify({'Pokemon': pokes[0]}),200
+
+@app.route('/api/v1/resources/pokemon/<int:pokemon_id>', methods=['DELETE'])
+def delete_task(pokemon_id):
+    pokes = [poke for poke in pokemons if str(poke['#']) == str(pokemon_id)]
+    if len(pokes) == 0:
+        print("failed")
+        abort(404)
+    pokemons.remove(pokes[0])
+    return jsonify({'result': True}),204
+
+def check_duplicates(pokemon):
+    for p in pokemons:
+        if p["Name"] == pokemon["Name"]: abort(400)
+
+app.run()
---
+++ test_api.py
@@ -0,0 +1,143 @@
+import requests
+import json
+
+
+def test_post_headers_body_json():
+    url = 'http://127.0.0.1:5000/api/v1/resources/pokemon'
+    
+    # Additional headers.
+    headers = {'Content-Type': 'application/json' } 
+
+    # Body
+    new_pokemon_positive = {
+        "Name": "Decodex",
+        "Type 1": "Grass",
+        "Type 2": "Poison",
+        "Total": "318",
+        "HP": "45",
+        "Attack": "49",
+        "Defense": "49",
+        "Sp. Atk": "65",
+        "Sp. Def": "65",
+        "Speed": "45",
+        "Generation": "1",
+        "Legendary": "False"
+    }
+    new_pokemon_duplicate={
+        "Name": "Bulbasaur",
+        "Type 1": "Grass",
+        "Type 2": "Poison",
+        "Total": "318",
+        "HP": "45",
+        "Attack": "49",
+        "Defense": "49",
+        "Sp. Atk": "65",
+        "Sp. Def": "65",
+        "Speed": "45",
+        "Generation": "1",
+        "Legendary": "False"
+    }
+    new_pokemon_negative = {
+        "Name": "Bazm",
+        "Type 1": "Grass",
+        "Type 2": "Poison",
+        "Total": "318",
+        "Attack": "49",
+        "Defense": "49",
+        "Sp. Atk": 4,
+        "Sp. Def": "65",
+        "Speed": "45",
+        "Legendary": "False"
+    }
+    # Performing the POST operations 
+    resp_pos = requests.post(url, headers=headers, data=json.dumps(new_pokemon_positive,indent=4))       
+    resp_dupl = requests.post(url, headers=headers, data=json.dumps(new_pokemon_duplicate,indent=4))       
+    resp_neg = requests.post(url, headers=headers, data=json.dumps(new_pokemon_negative,indent=4))       
+    
+    # Validate response headers and body contents, e.g. status code.
+    assert resp_pos.status_code == 201
+    resp_body = resp_pos.json()
+    assert resp_body['Pokemon']["Name"] == new_pokemon_positive['Name']
+
+    assert resp_dupl.status_code == 400
+    assert len(requests.get(url,params={"name":"Bulbasaur"}).json()) == 1 
+
+    assert resp_neg.status_code == 400
+
+    # print response full body as text
+    print(resp_pos.text,resp_neg.text)
+
+def test_get():
+    url = 'http://127.0.0.1:5000/api/v1/resources/pokemon'
+
+    #Should return one record with the name Bulbasaur
+    positive_args = url+ "?name=Bulbasaur"
+    #Should return more than one pokemon
+    positive_multiple_args = url+ "?name=Bulbasaur&hp=60"
+    # Should return only one
+    duplicate_args = url+ "?name=Bulbasaur&name=Bulbasaur"
+    #Strength is an invalid argument
+    invalid_args = url+ "?strength=45"
+
+    # Performing the api calls
+    resp_pos= requests.get(positive_args)
+    resp_pos_multiple= requests.get(positive_multiple_args)
+    resp_duplicate = requests.get(duplicate_args)
+    resp_invalid = requests.get(invalid_args)
+
+    assert resp_pos.status_code == 200
+    resp_body_pos = resp_pos.json()
+    assert resp_body_pos[0]["Name"] == "Bulbasaur"
+
+    assert resp_pos_multiple.status_code == 200
+    resp_pos_multiple = resp_pos_multiple.json()
+    print(resp_pos_multiple)
+    assert len(resp_pos_multiple) > 1 
+
+
+    assert resp_duplicate.status_code == 200
+    resp_body_dupl = resp_duplicate.json()
+    assert resp_body_dupl[0]["Name"] == "Bulbasaur"
+    assert len(resp_body_dupl) == 1 
+
+    assert resp_invalid.status_code == 404
+
+def test_put():
+    
+    url = 'http://127.0.0.1:5000/api/v1/resources/pokemon/1'
+    # Additional headers.
+    headers = {'Content-Type': 'application/json' } 
+
+    #Should update the hp to 125 for pokemon 1
+    positive = {"HP":"125"}
+
+    #Strength is an invalid argument
+    negative= {"Strength":"145"}
+
+    # Performing the api calls
+    resp_pos = requests.put(url,headers=headers, data=json.dumps(positive))
+    resp_neg = requests.put(url, headers=headers, data=json.dumps(negative))
+
+    assert resp_pos.status_code ==  200
+    assert resp_pos.json()["Pokemon"]["HP"] == positive["HP"]
+
+    assert resp_neg.status_code ==  400
+
+def test_delete():
+
+    url = 'http://127.0.0.1:5000/api/v1/resources/pokemon/1'
+
+    resp_pos = requests.delete(url)
+    resp_neg = requests.delete(url)
+
+    assert resp_pos.status_code == 204
+    assert requests.get(url).status_code == 404
+
+    assert resp_neg.status_code == 404
+
+
+
+
+
+if __name__ == '__main__': 
+    test_get()
